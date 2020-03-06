3a12f8f3096c0886046562bba9516cb3da89cf81
RAX7 <antoxa2013@gmail.com>
2020-03-06 15:21:48 +0700

    update .babelrc

--- advanced/vr/tasks/server.js
+++ advanced/vr/tasks/server.js
@@ -5,25 +5,20 @@ const babelify = require('babelify');
 const browserifyShader = require('browserify-shader');
 const getNextPort = require('get-next-port');
 
-const babelTransforms = babelify.configure({
-  presets: ['@babel/preset-env'],
-});
-
 const createServer = (port) => {
   budo('./src/index.js', {
-    live: true,             // live reload
-    dir:'dist',
-    serve:'bundle.js',
+    live: true, // live reload
+    dir: 'dist',
+    serve: 'bundle.js',
     stream: process.stdout, // log to stdout
-    port: port,             // use this as the base port
+    port: port, // use this as the base port
     browserify: {
-      transform: [babelTransforms, browserifyShader],
+      transform: [babelify, browserifyShader],
     }
   }).on('connect', function(ev) {
     //...
     console.log(`Server UP at ${port}`);
-  })  
+  })
 };
 
 getNextPort(9966).then(createServer);
-
